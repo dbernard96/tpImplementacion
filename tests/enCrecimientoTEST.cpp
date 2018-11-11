10326b13d67fb0940dd22f0bad43f1796ebb0558
David Bernard <davidbernard456@gmail.com>
2018-11-11 19:40:28 -0300

    actualize los test que habia que cambiar

--- tests/enCrecimientoTEST.cpp
+++ tests/enCrecimientoTEST.cpp
@@ -4,20 +4,71 @@
 
 using namespace std;
 
+
 TEST(enCrecimientoTEST, toroideDiagonalSeHaceTodoTrue){
-    toroide t1 = { 
-                  {true, false, false}, 
-                  {false, true, false}, 
-                  {false, false, true}};
+    toroide t1 = {
+            {true, false, false},
+            {false, true, false},
+            {false, false, true}};
+    bool res = enCrecimiento(t1);
+    EXPECT_FALSE(res);
+}
+
+TEST(enCrecimientoTEST, todoFalsoNoCrece){
+    toroide t1 = {
+            {false, false, false},
+            {false, false, false},
+            {false, false, false}};
+    bool res = enCrecimiento(t1);
+    EXPECT_FALSE(res);
+}
+
+TEST(enCrecimientoTEST, toroidePeriodicoNoCrece){
+    toroide t1 = {
+            {false, false, false, false},
+            {true,  true,  true,  false},
+            {false, false, false, false},
+            {false, false, false, false}};
+    bool res = enCrecimiento(t1);
+    EXPECT_FALSE(res);
+}
+
+TEST(enCrecimientoTEST, toroideLSeHaceBloque){
+    toroide t1 = {
+            {false, false, false, false},
+            {false, true,  true,  false},
+            {false, false, true,  false},
+            {false, false, false, false}};
+    bool res = enCrecimiento(t1);
+    EXPECT_FALSE(res);
+}
+
+TEST(enCrecimientoTEST, toroideExtremosEsBloque){
+    toroide t1 = {
+            {true,  false, false, true},
+            {false, false, false, false},
+            {false, false, false, false},
+            {true,  false, false, true}};
+    bool res = enCrecimiento(t1);
+    EXPECT_FALSE(res);
+}
+
+TEST(enCrecimientoTEST, toroideBloqueConUnoMasCrece){
+    toroide t1 = {
+            {false, false, true, false},
+            {false, true,  true,  false},
+            {false, true,  true,  false},
+            {false, false, false, false}};
     bool res = enCrecimiento(t1);
     EXPECT_TRUE(res);
 }
 
-TEST(enCrecimientoTEST, todoFalsoNoCrece){
-    toroide t1 = { 
-                  {false, false, false},
-                  {false, false, false},
-                  {false, false, false}}; 
+TEST(enCrecimientoTEST, toroideLineaDeCuatroCrece){
+    toroide t1 = {
+            {false, false, false, false},
+            {true,  true,  true,  true},
+            {false, false, false, false},
+            {false, false, false, false}};
     bool res = enCrecimiento(t1);
-    EXPECT_FALSE(res);
+    EXPECT_TRUE(res);
 }
